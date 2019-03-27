e057d028162ea61840cac480fb5bcc0a0842e940
vometablissement <hoechstesetablissement@gmail.com>
2019-03-27 08:48:35 +0100

    final release

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 const gm = require('gm').subClass({imageMagick:false});
 const app = express();
 const multer = require('multer');
+const isImage = require('is-image');
 const storage = multer.diskStorage({
     destination: __dirname,
     filename: function (req,file,cb){
@@ -23,16 +24,24 @@ app.get('/', function(req,res){
     res.send("hallo");
 });
 app.post('/api/file', upload.single(''), function (req,res,next){
-    gm(req.file.filename).write(__dirname+'/files/'+req.file.originalname,function (err) {
-        if(err) console.log(req.file);
-    });
-    gm(req.file.filename).resize(720,null).write(__dirname+'/files'+'/small_'+req.file.originalname,function (err) {
-        if(err) console.log(req.file);
-    });
-    gm(req.file.filename).resize(1280,null).write(__dirname+'/files'+'/medium_'+req.file.originalname,function (err) {
-        if(err) console.log(req.file);
-    });
-    gm(req.file.filename).resize(1920,null).write(__dirname+'/files'+'/big_'+req.file.originalname,function (err) {
-        if(err) console.log(req.file);
-    });
+    console.log(req.file);
+    if(isImage(req.file.filename)){
+        gm(req.file.filename).write(__dirname+'/files/'+req.file.originalname,function (err) {
+            if(err) console.log(req.file);
+        });
+        gm(req.file.filename).resize(720,null).write(__dirname+'/files'+'/small_'+req.file.originalname,function (err) {
+            if(err) console.log(req.file);
+        });
+        gm(req.file.filename).resize(1280,null).write(__dirname+'/files'+'/medium_'+req.file.originalname,function (err) {
+            if(err) console.log(req.file);
+        });
+        gm(req.file.filename).resize(1920,null).write(__dirname+'/files'+'/big_'+req.file.originalname,function (err) {
+            if(err) console.log(req.file);
+        });
+        res.status(200).send('ok');
+    }
+    else {
+       res.status(500).send('Server error');
+       console.log(res.statusCode);
+    }
 });
